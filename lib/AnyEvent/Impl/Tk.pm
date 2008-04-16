3c0881e2099c9fbb36b3ee6066c2838d74047cf9
root <root>
2008-04-16 14:39:00 +0000

    *** empty log message ***

--- lib/AnyEvent/Impl/Tk.pm
+++ lib/AnyEvent/Impl/Tk.pm
@@ -1,3 +1,41 @@
+=head1 NAME
+
+AnyEvent::Impl::Tk - AnyEvent adaptor for Tk
+
+=head1 SYNOPSIS
+
+  use AnyEvent;
+  use Tk;
+
+  # this module gets loaded automatically as required
+
+=head1 DESCRIPTION
+
+This module provides transparent support for AnyEvent. You don't have to
+do anything to make Tk work with AnyEvent except by loading Tk before
+creating the first AnyEvent watcher.
+
+Tk is buggy. Tk is extremely buggy. Tk is so unbelievably buggy that
+for each bug reported and fixed, you get one new bug followed by
+reintroduction of the old bug in a later revision. I regularly run out of
+words to describe how bad it really is.
+
+To work around the many, many bugs in Tk that don't get fixed, this
+adaptor dup()'s all filehandles that get passed into its I/O watchers,
+so if you register a read and a write watcher for one fh, AnyEvent will
+create two additional file descriptors (and handles).
+
+This creates a high overhead and is slow, but seems to work around all
+known bugs in L<Tk::fileevent>.
+
+To be able to access the Tk event loop, this module creates a main
+window and withdraws it immediately. This might cause flickering on some
+platforms, but Tk perversely requires a window to be able to wait for file
+handle readyness notifications. This window is always created (in this
+version of AnyEvent) and can be accessed as C<$AnyEvent::Impl::Tk::mw>.
+
+=cut
+
 package AnyEvent::Impl::Tk;
 
 no warnings;
@@ -5,7 +43,7 @@ use strict;
 
 use Tk ();
 
-my $mw = new MainWindow;
+our $mw = new MainWindow;
 $mw->withdraw;
 
 sub io {
@@ -72,5 +110,17 @@ sub one_event {
    Tk::DoOneEvent (0);
 }
 
-1
+1;
+
+=head1 SEE ALSO
+
+  L<AnyEvent>, L<Tk>.
+
+=head1 AUTHOR
+
+ Marc Lehmann <schmorp@schmorp.de>
+ http://home.schmorp.de/
+
+=cut
+
 
