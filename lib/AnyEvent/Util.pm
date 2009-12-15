5efe16bfa03714f14b1bc0e92566235aa7fc2d81
root <root>
2009-12-15 06:18:03 +0000

    *** empty log message ***

--- lib/AnyEvent/Util.pm
+++ lib/AnyEvent/Util.pm
@@ -110,74 +110,75 @@ Returns the empty list on any errors.
 
 =cut
 
-sub _win32_socketpair {
-   # perl's socketpair emulation fails on many vista machines, because
-   # vista returns fantasy port numbers.
+BEGIN {
+   if (AnyEvent::WIN32) {
+      *_win32_socketpair = sub () {
+         # perl's socketpair emulation fails on many vista machines, because
+         # vista returns fantasy port numbers.
 
-   for (1..10) {
-      socket my $l, &Socket::AF_INET, &Socket::SOCK_STREAM, 0
-         or next;
+         for (1..10) {
+            socket my $l, &Socket::AF_INET, &Socket::SOCK_STREAM, 0
+               or next;
 
-      bind $l, Socket::pack_sockaddr_in 0, "\x7f\x00\x00\x01"
-         or next;
+            bind $l, Socket::pack_sockaddr_in 0, "\x7f\x00\x00\x01"
+               or next;
 
-      my $sa = getsockname $l
-         or next;
+            my $sa = getsockname $l
+               or next;
 
-      listen $l, 1
-         or next;
+            listen $l, 1
+               or next;
 
-      socket my $r, &Socket::AF_INET, &Socket::SOCK_STREAM, 0
-         or next;
+            socket my $r, &Socket::AF_INET, &Socket::SOCK_STREAM, 0
+               or next;
 
-      bind $r, Socket::pack_sockaddr_in 0, "\x7f\x00\x00\x01"
-         or next;
+            bind $r, Socket::pack_sockaddr_in 0, "\x7f\x00\x00\x01"
+               or next;
 
-      connect $r, $sa
-         or next;
+            connect $r, $sa
+               or next;
 
-      accept my $w, $l
-         or next;
+            accept my $w, $l
+               or next;
 
-      # vista has completely broken peername/sockname that return
-      # fantasy ports. this combo seems to work, though.
-      #
-      (Socket::unpack_sockaddr_in getpeername $r)[0]
-      == (Socket::unpack_sockaddr_in getsockname $w)[0]
-         or (($! = WSAEINVAL), next);
+            # vista has completely broken peername/sockname that return
+            # fantasy ports. this combo seems to work, though.
+            #
+            (Socket::unpack_sockaddr_in getpeername $r)[0]
+            == (Socket::unpack_sockaddr_in getsockname $w)[0]
+               or (($! = WSAEINVAL), next);
 
-      # vista example (you can't make this shit up...):
-      #(Socket::unpack_sockaddr_in getsockname $r)[0] == 53364
-      #(Socket::unpack_sockaddr_in getpeername $r)[0] == 53363
-      #(Socket::unpack_sockaddr_in getsockname $w)[0] == 53363
-      #(Socket::unpack_sockaddr_in getpeername $w)[0] == 53365
+            # vista example (you can't make this shit up...):
+            #(Socket::unpack_sockaddr_in getsockname $r)[0] == 53364
+            #(Socket::unpack_sockaddr_in getpeername $r)[0] == 53363
+            #(Socket::unpack_sockaddr_in getsockname $w)[0] == 53363
+            #(Socket::unpack_sockaddr_in getpeername $w)[0] == 53365
 
-      return ($r, $w);
+            return ($r, $w);
+         }
+
+         ()
+      };
+
+      *portable_socketpair = \&_win32_socketpair;
+      *portable_pipe       = \&_win32_socketpair;
+   } else {
+      *portable_pipe = sub () {
+         my ($r, $w);
+
+         pipe $r, $w
+            or return;
+
+         ($r, $w);
+      };
+
+      *portable_socketpair = sub () {
+         socketpair my $fh1, my $fh2, &Socket::AF_UNIX, &Socket::SOCK_STREAM, &Socket::PF_UNSPEC
+            or return;
+
+         ($fh1, $fh2)
+      };
    }
-
-   ()
-}
-
-sub portable_pipe() {
-   return _win32_socketpair
-      if AnyEvent::WIN32;
-
-   my ($r, $w);
-
-   pipe $r, $w
-      or return;
-
-   ($r, $w);
-}
-
-sub portable_socketpair() {
-   return _win32_socketpair
-      if AnyEvent::WIN32;
-
-   socketpair my $fh1, my $fh2, &Socket::AF_UNIX, &Socket::SOCK_STREAM, &Socket::PF_UNSPEC
-      or return;
-
-   ($fh1, $fh2)
 }
 
 =item fork_call { CODE } @args, $cb->(@res)
@@ -763,148 +764,15 @@ Croaks when it cannot decode the string.
 
 =cut
 
-#sub punycode_encode($) {
-#   require "AnyEvent/Util/idna.pl";
-#   goto &punycode_encode;
-#}
-#
-#sub punycode_decode($) {
-#   require "AnyEvent/Util/idna.pl";
-#   goto &punycode_decode;
-#}
-
-# based on RFC 3492
-
-use AnyEvent (); BEGIN { AnyEvent::common_sense }
-use Carp ();
-use List::Util ();
-use integer;
-
-sub pyc_base         () {  36 }
-sub pyc_tmin         () {   1 }
-sub pyc_tmax         () {  26 }
-sub pyc_initial_bias () {  72 }
-sub pyc_initial_n    () { 128 }
-
-sub pyc_digits       () { "abcdefghijklmnopqrstuvwxyz0123456789" }
-
-sub pyc_adapt($$$) {
-   my ($delta, $numpoints, $firsttime) = @_;
-
-   $delta = $firsttime ? $delta / 700 : $delta >> 1;
-   $delta += $delta / $numpoints;
-
-   my $k;
-
-   while ($delta > (pyc_base - pyc_tmin) * pyc_tmax / 2) {
-      $delta /= pyc_base - pyc_tmin;
-      $k += pyc_base;
-   }
-
-   $k + $delta * (pyc_base - pyc_tmin + 1) / ($delta + 38)
-}
-
 sub punycode_encode($) {
-   my ($input) = @_;
-
-   my ($n, $bias, $delta) = (pyc_initial_n, pyc_initial_bias);
-
-   (my $output = $input) =~ y/\x00-\x7f//cd;
-   my $h = my $b = length $output;
-
-   my @input = split '', $input;
-
-   $output .= "-" if $b && $h < @input;
-
-   while ($h < @input) {
-      my $m = List::Util::min grep { $_ >= $n } map ord, @input;
-
-      $m - $n <= (0x7fffffff - $delta) / ($h + 1)
-         or Carp::croak "punycode_encode: overflow in punycode delta encoding";
-      $delta += ($m - $n) * ($h + 1);
-      $n = $m;
-
-      for my $i (@input) {
-         my $c = ord $i;
-         ++$delta < 0x7fffffff
-            or Carp::croak "punycode_encode: overflow in punycode delta encoding"
-            if $c < $n;
-
-         if ($c == $n) {
-            my ($q, $k) = ($delta, pyc_base);
-
-            while () {
-                my $t = List::Util::min pyc_tmax, List::Util::max pyc_tmin, $k - $bias;
-
-                last if $q < $t;
-
-                $output .= substr pyc_digits, $t + (($q - $t) % (pyc_base - $t)), 1;
-
-                $q = ($q - $t) / (pyc_base - $t);
-                $k += pyc_base;
-            }
-
-            $output .= substr pyc_digits, $q, 1;
-
-            $bias = pyc_adapt $delta, $h + 1, $h == $b;
-
-            $delta = 0;
-            ++$h;
-         }
-      }
-
-      ++$delta;
-      ++$n;
-   }
-
-   $output
+   require "AnyEvent/Util/idna.pl";
+   goto &punycode_encode;
 }
 
 sub punycode_decode($) {
-   my ($input) = @_;
-
-   my ($n, $bias, $i) = (pyc_initial_n, pyc_initial_bias);
-   my $output;
-
-   if ($input =~ /^(.*?)-([^-]*)$/x) {
-      $output = $1;
-      $input = $2;
-
-      $output =~ /[^\x00-\x7f]/
-         and Carp::croak "punycode_decode: malformed punycode";
-   }
-
-   while (length $input) {
-      my $oldi = $i;
-      my $w    = 1;
-
-      for (my $k = pyc_base; ; $k += pyc_base) {
-         (my $digit = index pyc_digits, substr $input, 0, 1, "")
-            >= 0
-            or Carp::croak "punycode_decode: malformed punycode";
-      
-         $i += $digit * $w;
-         
-         my $t = List::Util::max pyc_tmin, List::Util::min pyc_tmax, $k - $bias;
-         last if $digit < $t;
-
-         $w *= pyc_base - $t;
-      }
-
-      my $outlen = 1 + length $output;
-      $bias = pyc_adapt $i - $oldi, $outlen, $oldi == 0;
-
-      $n += $i / $outlen;
-      $i %=      $outlen;
-
-      substr $output, $i, 0, chr $n;
-      ++$i;
-   }
-
-   $output
+   require "AnyEvent/Util/idna.pl";
+   goto &punycode_decode;
 }
-
-1
 
 =item AnyEvent::Util::idn_nameprep $idn[, $display]
 
