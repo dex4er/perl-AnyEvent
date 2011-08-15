97fba65693697a5561576840d469245a03720f55
root <root>
2011-08-15 23:21:09 +0000

    *** empty log message ***

---
+++ lib/AnyEvent/Log.pm
@@ -0,0 +1,32 @@
+=head1 NAME
+
+AnyEvent::Log - simple logging "framework"
+
+=head1 SYNOPSIS
+
+   use AnyEvent::Log;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=over 4
+
+=cut
+
+package AnyEvent::Log;
+
+use POSIX ();
+
+use AnyEvent (); BEGIN { AnyEvent::common_sense }
+
+1;
+
+=back
+
+=head1 AUTHOR
+
+ Marc Lehmann <schmorp@schmorp.de>
+ http://home.schmorp.de/
+
+=cut
