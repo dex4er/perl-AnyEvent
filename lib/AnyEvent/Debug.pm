818736150aecc32a8528d0d7d459c1f8d6b694ce
root <root>
2011-10-04 15:35:26 +0000

    *** empty log message ***

--- lib/AnyEvent/Debug.pm
+++ lib/AnyEvent/Debug.pm
@@ -427,7 +427,6 @@ sub backtrace(;$) {
 
    my (@bt, @c);
    my ($modlen, $sub);
-   local $_;#d#
 
    for (;;) {
       #         0          1      2            3         4           5          6            7       8         9         10
